bfc280ed4ee07286c0bf689450e663f2922e409c
scarlet-project <mauricioazevedogou@gmail.com>
2023-01-27 11:55:42 -0300

    uhu

---
+++ Exercicios/ex011.py
@@ -0,0 +1,8 @@
+l = float(input('Largura da parede:'))
+a = float(input('Altura da parede:'))
+a = float(l*a)
+
+print(f'{"Calcular":=^20}')
+print(f'Dimensão: [{l}] x [{a}]')
+print((f'Área: [{a:.2f}M²]'))
+print(f'Tinta: [{a/2:.2f}L]')
