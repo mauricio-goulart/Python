c81c6749497edb207b4f2806736f789fcf25bb99
scarlet-project <mauricioazevedogou@gmail.com>
2023-01-28 11:46:12 -0300

    update aula08

--- Aulas/aula08.py
+++ Aulas/aula08.py
@@ -1,7 +1,11 @@
 import math
+import random
 
 n1 = int(input('Digite um número: '))
 raiz = math.sqrt(n1)
 
 print(f'A raiz quadrada de {n1} é {math.sqrt(n1)}')
 print(raiz)
+
+n2 = random.randint(1, 10)
+print(n2)
