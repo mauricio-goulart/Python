a740e8001705e82b6c12d326431704721cdbb228
scarlet-project <mauricioazevedogou@gmail.com>
2023-01-28 10:41:56 -0300

    update ex015

--- Exercicios/ex015.py
+++ Exercicios/ex015.py
@@ -2,5 +2,5 @@ d = int(input('Dias:'))
 k = float(input('Km:'))
 
 print(f'{"TOTAL A PAGAR":=^20}')
-print(f'Pagar: [{d * 60 + k * 0.15}]')
+print(f'Pagar: [{d * 60 + k * 0.15:.2f}]')
 print(20 * '=')
