cb23e45403008fa3902102b84b171adc9b9dc4b4
scarlet-project <mauricioazevedogou@gmail.com>
2023-01-30 16:58:26 -0300

    update ex022

--- Exercicios/ex022.py
+++ Exercicios/ex022.py
@@ -1,10 +1,10 @@
-nome = str(input('Digite o seu nome: '))
-n = nome.strip()
-nm = n.split()
+nome = str(input('Digite o seu nome: ')).strip()
+
+nm = nome.split()
 
 print(f'{"Analisando":=^20}')
 print(f'Maiúsculo:[{nome.upper()}]')
 print(f'Minúsculo:[{nome.lower()}]')
-print(f'Total Letras:[{len(n) - n.count(" ")}]')
+print(f'Total Letras:[{len(nome) - nome.count(" ")}]')
 print(f'Total Primeiro nome:[{len(nm[0])}]')
 print('=' * 20)
