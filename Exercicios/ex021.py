b186388255418352a4df201afbcbd2dfb804f295
scarlet-project <mauricioazevedogou@gmail.com>
2023-01-29 23:10:12 -0300

    update021

--- Exercicios/ex021.py
+++ Exercicios/ex021.py
@@ -1,3 +1,4 @@
 from playsound import playsound
 print(f'{"MUSICA":=^20}')
 playsound('OQIGAP_ MC IG, Davi, Ryan SP, Hariel, Don Juan, Cebezinho, NK, MC Rick e Tigre (DJ Oreia).mp3')
+print('=' * 20)
