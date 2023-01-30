ecbb5ada9e75135bf39db266353fe748f5723c65
scarlet-project <mauricioazevedogou@gmail.com>
2023-01-29 22:50:45 -0300

    update ex020

--- Exercicios/ex020.py
+++ Exercicios/ex020.py
@@ -1,11 +1,14 @@
 import random
 
-a1 = input('Nome do Aluno: ')
-a2 = input('Nome do Aluno: ')
-a3 = input('Nome do Aluno: ')
-a4 = input('Nome do Aluno: ')
+a1 = str(input('Nome do Aluno: '))
+a2 = str(input('Nome do Aluno: '))
+a3 = str(input('Nome do Aluno: '))
+a4 = str(input('Nome do Aluno: '))
 
-list = 'a1 a2 a3 a4'.split()
+print(f'{"ORDEM DE APRESENTAÇÃO":=^40}')
+
+list = [a1, a2, a3, a4]
 esc = random.shuffle(list)
 
 print(list)
+print('=' * 40)
