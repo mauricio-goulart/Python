3e05c3b0d60c58e26e6abf8339c441189bce8a2a
scarlet-project <mauricioazevedogou@gmail.com>
2023-01-31 12:00:17 -0300

    Update ex028.py

--- Exercicios/ex028.py
+++ Exercicios/ex028.py
@@ -1,9 +1,9 @@
 import random
 import time
 
+print('-=-' * 20)
 print('Vou pensar em um número de 0 a 5. Tente adivinhar...')
-
-print('=' * 50)
+print('-=-' * 20)
 
 pc = int(random.randint(0, 5))
 h = int(input('Em que número eu pensei? :'))
