f324cbfe3d6cb9200ca0c54ce9cca0534b7d3f2a
scarlet-project <mauricioazevedogou@gmail.com>
2023-01-31 12:18:03 -0300

    Update ex029.py

--- Exercicios/ex029.py
+++ Exercicios/ex029.py
@@ -1,3 +1,6 @@
 n1 = int(input('Velocidade do carro: '))
 print(f'{"Radar":=^20}')
 if n1 > 80 :
+    print(f'Multado! Limite [80]Km. Total a Pagar:[{(n1 - 80) * 7}]')
+else:
+    print(f'Excelente! Tenha um ótimo dia!')
