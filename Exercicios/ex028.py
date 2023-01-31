15ce00bcb8b49ab772d59e41ddaf81be768b06fd
scarlet-project <mauricioazevedogou@gmail.com>
2023-01-31 16:54:21 -0300

    Update ex028.py

--- Exercicios/ex028.py
+++ Exercicios/ex028.py
@@ -1,17 +1,19 @@
 import random
 import time
 
-print('-=-' * 20)
-print('Vou pensar em um número de 0 a 5. Tente adivinhar...')
-print('-=-' * 20)
+print('\33[35m-=-' * 20)
+print('\33[34mVou pensar em um número de 0 a 5. Tente adivinhar...')
+print('\33[35m-=-' * 20)
 
 pc = int(random.randint(0, 5))
-h = int(input('Em que número eu pensei? :'))
-print('PROCESSANDO...')
+h = int(input('\33[34mEm que número eu pensei? :'))
+print('\33[33mPROCESSANDO...')
 time.sleep(2)
 
 if h == pc :
-    print(f'Parabens!!! Você ganhou eu pensei no {pc}')
+    print(f'\33[32mParabens!!! Você ganhou eu pensei no {pc}')
 else:
-    print(f'Perdeu!!! Você errou eu pensei no {pc}')
-print('=' * 40)
+    print(f'\33[31mPerdeu!!! Você errou eu pensei no {pc}')
+print('\33[35m=' * 40)
+
+input('\33[mdigite enter para encerrar')
