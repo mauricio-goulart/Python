2db9254e71ee54821a4d88bf43ac43f89a585735
scarlet-project <mauricioazevedogou@gmail.com>
2023-01-31 13:13:05 -0300

    Update ex034.py

--- Exercicios/ex034.py
+++ Exercicios/ex034.py
@@ -1,3 +1,6 @@
 sala = float(input('Digite seu salário: R$'))
 print(f'{"Reajuste":=^20}')
-if sala > 1200
+if sala > 1200 :
+    print(f'Salario reajustado:[R${(sala * 15 / 100) + sala}]')
+else:
+    print(f'Salário reajustado:[R${(sala * 15 / 100) + sala}]')
