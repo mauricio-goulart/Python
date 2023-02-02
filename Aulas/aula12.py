b710db40be4066a44146f167077bc5218b7a0195
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-02 10:16:18 -0300

    Update aula12.py

---
+++ Aulas/aula12.py
@@ -0,0 +1,10 @@
+nome = str(input('Digite seu nome: '))
+
+if nome == 'Mauricio' or nome == 'Maria':
+    print('Que nome lindo')
+elif nome == 'Carlos' or nome == 'Petry':
+    print('Nome bem normal')
+else:
+    print('Nome bem normal')
+
+print(f'Bom dia {nome}')
