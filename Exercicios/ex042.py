5f2f7caa9cc084c6e5c659cb9b8a20c2b4d23782
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-02 13:03:48 -0300

    Update ex042.py

--- Exercicios/ex042.py
+++ Exercicios/ex042.py
@@ -3,7 +3,15 @@ r2 = float(input('Tamanho da reta: '))
 r3 = float(input('Tamanho da reta: '))
 print(f'{"Analisando":=^20}')
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r2 + r1:
-    print('Formam um [Triangulo]')
+    print('Formam um [Triangulo]', end='')
+    if r1 == r2 == r3 :
+        print('[EQUILÁTERO]')
+    elif r1 != r2 != r3 != r1 :
+        print('[ESCALENO]')
+    elif r1 != r2 != r3:
+        print('[ISÓSCELES]')
+
+
 else:
     print('Não forma')
 print('=' * 20)
