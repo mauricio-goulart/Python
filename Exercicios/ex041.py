a53af53e9cdeaa2ede14722b41b5232e26a67151
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-02 12:54:21 -0300

    Update ex041.py

--- Exercicios/ex041.py
+++ Exercicios/ex041.py
@@ -5,13 +5,20 @@ idade = date.today().year - i
 print('-=' * 15)
 
 if idade <= 9 :
-    print('Mirim')
+    print(f'Idade:[{idade}]')
+    print('Categoria:[Mirim]')
 elif idade <= 14 :
-    print('Infantil')
+    print(f'Idade:[{idade}]')
+    print('Categoria:[Infantil]')
 elif idade <=19 :
-    print('Junior')
+    print(f'Idade:[{idade}]')
+    print('Categoria:[Junior]')
 elif idade <= 20 :
-    print('Senior')
+    print(f'Idade:[{idade}]')
+    print('Categoria:[Senior]')
 else:
-    print('Senior')
+    print(f'Idade:[{idade}]')
+    print('Categoria:[Master]')
+
+print('-=' * 15)
 
