8d3d65da93678bcbb5942c64973f33ca9c49c5ad
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-02 11:44:19 -0300

    Update ex038.py

--- Exercicios/ex038.py
+++ Exercicios/ex038.py
@@ -1,3 +1,14 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 print('-=' * 15)
+
+if n1 > n2 :
+    print(f'Maior:[{n1}]')
+    print(f'Menor:[{n2}]')
+
+elif n2 > n1 :
+    print(f'Maior:[{n1}]')
+    print(f'Menor:[{n2}]')
+
+else:
+    print('[IGUAIS]')
