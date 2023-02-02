44899cf39bd5f98387b7445bbdf94a8820e1668d
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-02 18:28:11 -0300

    update

---
+++ Exercicios/ex046.py
@@ -0,0 +1,9 @@
+from time import sleep
+print('-=' * 10)
+print('Contagem Regressiva')
+print('-=' * 10)
+
+for contador in range(10, 0 - 1 , -1):
+    print(contador)
+    sleep(1)
+print('Feliz Ano Novo!!!!!')
