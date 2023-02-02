bb1111f9df3f1501f89d7b8da60d7dc11922de97
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-02 15:50:32 -0300

    Update ex045.py

---
+++ Exercicios/ex045.py
@@ -0,0 +1,39 @@
+from random import randint
+itens = ('Pedra' , 'Papel', 'Tesoura')
+computador = itens[randint(0,2)]
+
+print(f'{"JOKENPO":=^20}')
+print('[ 0 ] Pedra')
+print('[ 1 ] Papel')
+print('[ 2 ] Tesoura')
+jogada = int(input('Sua jogada: '))
+jogador = itens[jogada]
+print('-=' * 10)
+print(jogador)
+print(computador)
+
+if computador == '0': #pc pedra
+    if jogador == 1:
+        print('Jogador Ganhou')
+    elif jogador == 2:
+        print('Jogador Perdeu')
+    elif jogador == 0:
+        print('EMPATE')
+    else:
+        print('JOGADA INVALIDA')
+
+elif computador == '1': #pc papel
+    if jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('Jogador Ganhou')
+    elif jogador == 0:
+        print('Jogador Perdeu')
+
+elif computador == '2':
+    if jogador == 1:
+        print('Jogador Perdeu')
+    elif jogador == 2:
+        print('EMPATE')
+    elif jogador == 0:
+        print('Jogador Ganhou')
