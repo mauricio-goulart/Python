1483575428fd2c2c9457ac150f0b509616a13e80
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-02 18:33:00 -0300

    Update ex047.py

--- Exercicios/ex047.py
+++ Exercicios/ex047.py
@@ -2,7 +2,7 @@ print('-=' * 15)
 print('Números Pares do 0 a 50')
 print('-=' * 15)
 
-for contador in range(0,50+1):
+for contador in range(1,51):
     if contador % 2 == 0:
         print(contador, end=' ')
 print('Acabou!!!')
