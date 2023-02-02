62b6f92352ab95b7503e53ca523a4b9e09e00d07
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-02 18:58:32 -0300

    Update ex049.py

---
+++ Exercicios/ex049.py
@@ -0,0 +1,12 @@
+from time import sleep
+cont = int(0)
+print('-=' * 15)
+m = int(input('Digite o número: '))
+print('-=' * 15)
+
+
+for contador in range(0,11):
+    print(f'{m} x {cont}')
+    cont = cont + 1
+    sleep(0.5)
+print('-=' * 15)
