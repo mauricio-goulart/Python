490df7a83b4534743acecdb8cc67865c58f7001e
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-02 12:21:02 -0300

    Update ex039.py

--- Exercicios/ex039.py
+++ Exercicios/ex039.py
@@ -1,7 +1,17 @@
+from datetime import date
 print('Alistamento Militar')
 print('-=' * 15)
 
-i = int(input('Digite sua idade: '))
+i = int(input('Data de nascimento: '))
 
-if 2022 - i == 2005:
-    print('Bora')
+if date.today().year - i == 18 :
+    print(f'Idade:[{date.today().year - i}]')
+    print(f'Está na hora de se ALISTAR')
+elif date.today().year - i < 18 :
+    print(f'Idade:[{date.today().year - i}]')
+    print(f'Ainda não está na hora')
+    print(f'Falta:[{ 18-(2023 - i )}] Anos')
+elif date.today().year - i > 18 :
+    print(f'Você está [{(2023 - i) - 18}] anos atrasado')
+
+print('=-' * 15)
