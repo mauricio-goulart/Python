b0c8b1344d71576b443dde859192b83714098009
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-03 10:19:09 -0300

    Update ex051.py

--- Exercicios/ex051.py
+++ Exercicios/ex051.py
@@ -4,7 +4,8 @@ print('-=' * 15)
 
 n1 = int(input('Primeiro termo:'))
 r = int(input('Razão:'))
+d = n1 + (10-1) * r
 
-for contador in range(n1,10* r,r):
-    print(contador)
+for contador in range(n1,d + r,r):
+    print(contador, end=' ')
 
