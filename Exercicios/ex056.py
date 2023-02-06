489ff1b5b5239017ec4f1f962648e4b45998149e
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-06 19:36:12 -0300

    Update ex056.py

--- Exercicios/ex056.py
+++ Exercicios/ex056.py
@@ -1,6 +1,12 @@
 m = float(0)
-for contador in range(1,6):
+idade = float(0)
+soma = float(0)
+nomev = ''
+for contador in range(1,5):
     print(f'----- {contador}º PESSOA -----')
     nome = str(input('Nome: ')).strip()
-    Idade = int(input('Idade: '))
-    Sexo = str(input('Sexo [M/F]: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    soma = soma + idade
+
+media = idade / 4
