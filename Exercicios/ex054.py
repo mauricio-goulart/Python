0d4465e4522ef65434ebe09c834b9211efb0d405
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-06 19:04:46 -0300

    Update ex054.py

--- Exercicios/ex054.py
+++ Exercicios/ex054.py
@@ -1,4 +1,10 @@
 maior = int(0)
 menor = int(0)
 for contador in range(1,8):
-    pessoa = int(input(f'Digite a idade da {contador}ºPessoa'))
+    pessoa = int(input(f'Ano de nascimento [{contador}º]Pessoa:'))
+    if pessoa >2005:
+        maior = maior + 1
+        print(f'{maior} são menores de idade')
+    else:
+        menor = menor + 1
+        print(f'{menor} são maiores de idade')
