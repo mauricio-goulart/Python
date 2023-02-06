e52ec745559033aa47452a821bae6c75babe67ba
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-06 19:29:59 -0300

    Update ex056.py

---
+++ Exercicios/ex056.py
@@ -0,0 +1,6 @@
+m = float(0)
+for contador in range(1,6):
+    print(f'----- {contador}º PESSOA -----')
+    nome = str(input('Nome: '))
+    Idade = int(input('Idade: '))
+    Sexo = str(input('Sexo [M/F]: ')).strip()
