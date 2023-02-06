bb875ceb78572fbec7fd61aa2b70f38b37e542a5
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-06 17:53:10 -0300

    Update ex052.py

--- Exercicios/ex052.py
+++ Exercicios/ex052.py
@@ -1,5 +1,10 @@
 num = int(input('Digite um Número: '))
 
 for contador in range(1, num + 1):
-    if num % num == 0 and num % 1 == 0:
-        print(num)
+
+    if num % contador == 0:
+        print('\033[34m', end=' ')
+    else:
+        print('\033[m', end=' ')
+
+    print(contador, end=' ')
