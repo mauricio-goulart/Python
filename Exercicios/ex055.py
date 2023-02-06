cd5553172a884bd2136d670d4e8a61c66a3a8241
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-06 19:21:04 -0300

    Update ex055.py

--- Exercicios/ex055.py
+++ Exercicios/ex055.py
@@ -2,6 +2,15 @@ maior = 0
 menor = 0
 for p in range(1,6):
     peso = float(input(f'Digite o peso da {p}º: '))
-    if peso > maior:
+    if p == 1:
         maior = peso
-    if peso
+        menor = peso
+    else:
+        if peso > maior:
+         maior = peso
+        if peso < menor:
+            menor = peso
+print('-=' * 15)
+print(f'Menor peso:[{menor}]')
+print(f'Maior peso:[{maior}]')
+print('-=' * 15)
