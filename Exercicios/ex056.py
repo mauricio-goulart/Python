81057a5e5e14493b3b1d2ed1fbe0a14d925c2a12
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-06 19:36:40 -0300

    Update ex056.py

--- Exercicios/ex056.py
+++ Exercicios/ex056.py
@@ -2,11 +2,13 @@ m = float(0)
 idade = float(0)
 soma = float(0)
 nomev = ''
+maioridadeh = 0
 for contador in range(1,5):
     print(f'----- {contador}º PESSOA -----')
     nome = str(input('Nome: ')).strip()
     idade = int(input('Idade: '))
     sexo = str(input('Sexo [M/F]: ')).strip()
     soma = soma + idade
+
 
 media = idade / 4
