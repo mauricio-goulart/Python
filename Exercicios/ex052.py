2b4a9e91f9573268cf21d8923c02da7b6294466a
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-06 18:10:53 -0300

    Update ex052.py

--- Exercicios/ex052.py
+++ Exercicios/ex052.py
@@ -1,10 +1,18 @@
 num = int(input('Digite um Número: '))
-
+tot = int(0)
+print('-=' * 20)
 for contador in range(1, num + 1):
 
     if num % contador == 0:
         print('\033[34m', end=' ')
+        tot = tot + 1
     else:
         print('\033[m', end=' ')
 
-    print(contador, end=' ')
+    print(f'{contador}',end=' ')
+
+print('\n\033[m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+
+if tot > 2:
+    print(f'[PRIMO:[NÃO]')
+
