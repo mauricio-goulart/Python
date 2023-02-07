b7c1c093fa9ea8ee3319bb9df00f4a459a086a1f
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-07 19:10:28 -0300

    Update ex059.py

--- Exercicios/ex059.py
+++ Exercicios/ex059.py
@@ -1,10 +1,12 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 sair = False
-
+d = ''
+print('-=' * 15)
 while not sair:
     print('\t[ 1 ] Somar')
     print('\t[ 2 ] Multiplicar')
     print('\t[ 3 ] Maior')
     print('\t[ 4 ] Novos números')
     print('\t[ 5 ] Sair do programa')
+    d = str(input('>>>> Qual a sua opção: ')).strip()
