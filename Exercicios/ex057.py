d3f0f8b74d8cc195819882d4899eb61a038b2f04
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-07 17:15:43 -0300

    Update ex057.py

--- Exercicios/ex057.py
+++ Exercicios/ex057.py
@@ -3,6 +3,9 @@ c = 0
 while r == 's':
     c = c + 1
     print(f'---- {c}ºPESSOA ----')
-    sexo = str(input('Sexo:[M/F]'))
+    sexo = str(input('Sexo:[M/F]')).upper()
+    if sexo != 'M' and sexo != 'F':
+        print('DIGITE UM SEXO VÁLIDO:')
+
 
     r = str(input('Continuar: [s/n]'))
