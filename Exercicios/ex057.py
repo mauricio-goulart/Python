b8d214e308577bc8ed6f3552b61cf4eb2545036d
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-07 17:24:26 -0300

    Update ex057.py

--- Exercicios/ex057.py
+++ Exercicios/ex057.py
@@ -1,6 +1,6 @@
-sexo = str(input('Digite o seu sexo: [M/F]')).upper()
+sexo = str(input('Digite o seu sexo: [M/F]')).upper().strip()
 
 while sexo != 'M' and sexo != 'F':
-    sexo = str(input('Dados Invalidos, Digite um sexo valido: [M/F]')).upper()
+    sexo = str(input('Dados Invalidos, Digite um sexo valido: [M/F]')).upper().strip()
 
 print(f'Sexo {sexo} registrado com sucesso!')
