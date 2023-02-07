c7372bb9df14fa905abe124b0463ff8694f6bdfe
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-07 12:56:47 -0300

    Update aula14.py

--- Aulas/aula14.py
+++ Aulas/aula14.py
@@ -2,5 +2,5 @@ n = 1
 r = 'S'
 while r == 'S':
     n =  int(input('Digite um valor: '))
-    r = input('Quer continuar: [S/N]').upper()
+    r = str(input('Quer continuar: [S/N]')).upper()
 print('fim')
