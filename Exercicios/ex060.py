8a020a762cb3e9dc0be2fcd4c07d734ccdfe2161
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-08 12:10:13 -0300

    Update ex060.py

--- Exercicios/ex060.py
+++ Exercicios/ex060.py
@@ -4,4 +4,13 @@ f = 1
 
 print(f'Calculando [{n}]!:')
 while c > 0 :
-    print({f'{c}')
+    print(f'{c}')
+    if c > 1:
+        print(' x ')
+    else:
+        print(' = ')
+
+    f = f * c
+    c = c - 1
+print(f'{f}')
+
