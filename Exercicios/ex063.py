1fbe53cc5afc130e5bc21d12665dcc08b6a8e57e
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-08 12:50:05 -0300

    Update ex063.py

---
+++ Exercicios/ex063.py
@@ -0,0 +1,12 @@
+print('-=' * 15)
+print('Sequencia Fibonacci')
+print('-=' * 15)
+n = int(input('Quantos termos você quer mostrar?: '))
+t1 = 0
+t2 = 1
+print('~' * 30)
+print(f'{t1}, {t2}', end='')
+cont = 3
+
+while cont <= n:
+    t3 = 
