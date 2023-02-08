c9f3ae11a30276ffa49c0aeec3635cff7b401cf1
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-08 12:11:46 -0300

    Update ex060.py

--- Exercicios/ex060.py
+++ Exercicios/ex060.py
@@ -2,15 +2,15 @@ n = int(input('Digite um número para calcular seu fatorial: '))
 c = n
 f = 1
 
-print(f'Calculando [{n}]!:')
+print(f'Calculando [{n}]!:', end=' ')
 while c > 0 :
-    print(f'{c}')
+    print(f'{c}', end=' ')
     if c > 1:
-        print(' x ')
+        print(' x ', end=' ')
     else:
-        print(' = ')
+        print(' = ', end=' ')
 
     f = f * c
     c = c - 1
-print(f'{f}')
+print(f'{f}', end=' ')
 
