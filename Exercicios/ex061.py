bec7c670d52a202d920e2062433165dfd312f944
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-08 12:24:20 -0300

    Update ex061.py

--- Exercicios/ex061.py
+++ Exercicios/ex061.py
@@ -1,3 +1,10 @@
 print('-=' * 15)
 print('\t10 TERMOS DE UMA PA')
 print('-=' * 15)
+
+primeiro = int(input('Primeiro Termo: '))
+termo = int(input('Razão da PA: '))
+cont = 1
+
+while cont <= 10:
+    print(f'{termo}', end=' ')
