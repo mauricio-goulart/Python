3a7baa80fd21948f1e79436b112b87fc7bf6d95e
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-08 12:54:48 -0300

    Update ex063.py

--- Exercicios/ex063.py
+++ Exercicios/ex063.py
@@ -5,7 +5,7 @@ n = int(input('Quantos termos você quer mostrar?: '))
 t1 = 0
 t2 = 1
 print('~' * 30)
-print(f'{t1}, {t2}', end='')
+print(f'{t1} > {t2}', end='')
 cont = 3
 
 while cont <= n:
@@ -15,4 +15,4 @@ while cont <= n:
     t2 = t3
     cont = cont + 1
 
-print('FIM')
+print('\nFIM')
