b3a98021803c104c8f717496da4df706a72c76b0
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-08 18:26:25 -0300

    Update ex067.py

--- Exercicios/ex067.py
+++ Exercicios/ex067.py
@@ -4,15 +4,11 @@ cont = 0
 n = int(input('Quer ver a tabuada de qual valor? '))
 print('-=' * 15)
 while True:
-
     for cont in range(0,11):
         print(f'\t{n} x {cont} = {n * cont}')
-
     print('-=' * 15)
-
     n = int(input('Quer ver a tabuada de qual valor? '))
     if n < 0:
         break
-
 print('-=' * 15)
 print('PROGRAMA DE TABUADA ENCERRADA')
