7a8e104e2517a18a7ad26c60ac82fa9fd6390377
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-08 14:40:51 -0300

    Update ex064.py

--- Exercicios/ex064.py
+++ Exercicios/ex064.py
@@ -1,4 +1,10 @@
 n = 0
 cond = False
+cont = 0
+soma = 0
 while not cond:
-    n
+    if n != 999:
+        n = int(input('Digite um número [999 para parar]: '))
+        cont = cont + 1
+        soma = soma + n
+
