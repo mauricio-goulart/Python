0b89933d2c7610aec72de328b48270d5641ff6e5
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-08 12:31:25 -0300

    Update ex062.py

--- Exercicios/ex062.py
+++ Exercicios/ex062.py
@@ -6,8 +6,14 @@ primeiro = int(input('Primeiro Termo: '))
 razao = int(input('Razão da PA: '))
 cont = 1
 termo = primeiro
-while cont <= 10:
-    print(f'{termo}', end=' ')
-    termo = termo + razao
-    cont = cont + 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print(f'{termo}', end=' ')
+        termo = termo + razao
+        cont = cont + 1
+    print('PAUSA')
+    mais = int(input('Quantos termos a mais?: '))
 print('FIM')
