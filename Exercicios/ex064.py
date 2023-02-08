9860d5322ffe15828cf8886c2cd74247e85c709f
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-08 14:48:55 -0300

    Update ex064.py

--- Exercicios/ex064.py
+++ Exercicios/ex064.py
@@ -1,10 +1,12 @@
-n = 0
-cond = False
+
 cont = 0
 soma = 0
-while not cond:
-    if n != 999:
-        n = int(input('Digite um número [999 para parar]: '))
-        cont = cont + 1
-        soma = soma + n
+n = int(input('Digite um número [999 para parar]: '))
+while n != 999:
+    cont = cont + 1
+    soma = soma + n
+    n = int(input('Digite um número [999 para parar]: '))
 
+print('-=' * 15)
+print(f'Você digitou [{cont}] vezes e a soma entre eles são [{soma}]')
+print('-=' * 15)
