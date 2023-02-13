3c6d4deabd8b0cf5c625b9787b205878cc5fc522
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-13 18:51:34 -0300

    Update ex070.py

--- Exercicios/ex070.py
+++ Exercicios/ex070.py
@@ -1,8 +1,11 @@
 print('\tLOJA SUPER BARATO\n')
 c = 1
-
+totp = 0
+maiorp = 0
+nomemenor = ''
 while True:
     print(f'----- {c}ºPRODUTO -----')
     c = c + 1
     nomep = str(input('Nome: '))
     precop = str(input('Preço: '))
+    if
