4050c74524162c1a85fee9750ce5a1a5e719a062
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-13 18:22:01 -0300

    Update ex069.py

--- Exercicios/ex069.py
+++ Exercicios/ex069.py
@@ -1,11 +1,14 @@
 c = 1
 cond = ''
 sexo = ''
+midade = 0
+toth = 0
 while True:
     print(f'---- {c}ºPessoa ----')
     c = c + 1
     nome = str(input('Nome: '))
-    sexo = str(input('Sexo: [M/F] '))
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo: [M/F] ')).strip().lower()
     while sexo not in 'MmFf':
         print('SEXO INVALIDO')
         sexo = str(input('Sexo: [M/F] '))
@@ -13,5 +16,9 @@ while True:
     while cond not in 'SsnN':
         print('DIGITE CERTO')
         cond = str(input('Quer continuar?: [S/N] ')).strip().lower()
+    if idade > 18:
+        midade = midade + 1
+    if sexo == 'm'
     if cond == 'n':
         break
+
