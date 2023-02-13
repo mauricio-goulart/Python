cfa0336db2ecee5cbc428ac7595ef2d526599564
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-12 21:45:00 -0300

    Update ex068.py

--- Exercicios/ex068.py
+++ Exercicios/ex068.py
@@ -1,12 +1,3 @@
 from random import randint
 from time import sleep
-impar = 0
-par = 0
-print('=-' * 15)
-print('\tÍMPAR OU PAR')
-print('=-' * 15)
-pc = randint(0, 10)
-h = int(input('Digite um valor: '))
-dec = input('Impar ou Par:[I/P]')
-print('=-' * 15)
-conta = (pc + h) % 2
+v - 0
