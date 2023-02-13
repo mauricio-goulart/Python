ebfa49e92d5b0a913299b6a0719845512827354e
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-12 22:01:32 -0300

    Update ex068.py

--- Exercicios/ex068.py
+++ Exercicios/ex068.py
@@ -5,12 +5,13 @@ while True:
     jogador = int(input('Diga um valor: '))
     computador = randint(0,10)
     total = computador + jogador
-    tipo = ''
-    while tipo == 'P' and tipo == 'I':
+    tipo = ' '
+    while tipo != 'P' and tipo != 'I':
         tipo = str(input('Par ou Impar: [P/I]')).strip().upper()
     print(f'Você:[{jogador}]')
     print(f'Computador:[{computador}]')
     print(f'Total:[{total}]')
+    print('DEU PAR' if total % 2 == 0 else 'DEU IMPAR')
     if tipo == 'P':
         if total % 2 == 0:
             print('Voce, VENCEU!!')
