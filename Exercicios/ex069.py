52a8aa1b674aa5acd121b3c8d189209d64658b10
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-13 18:27:22 -0300

    Update ex069.py

--- Exercicios/ex069.py
+++ Exercicios/ex069.py
@@ -3,6 +3,7 @@ cond = ''
 sexo = ''
 midade = 0
 toth = 0
+fidade = 0
 while True:
     print(f'---- {c}ºPessoa ----')
     c = c + 1
@@ -18,7 +19,14 @@ while True:
         cond = str(input('Quer continuar?: [S/N] ')).strip().lower()
     if idade > 18:
         midade = midade + 1
-    if sexo == 'm'
+    if sexo == 'm':
+        toth = toth + 1
+    if sexo == 'f' and idade < 20:
+        fidade = fidade + 1
     if cond == 'n':
         break
-
+print('-' * 20)
+print(f'Maiores de idade: [{midade}]')
+print(f'Total Homens: [{toth}]')
+print(f'Mulheres menor de 20: [{fidade}]')
+print('-' * 20)
