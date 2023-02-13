6106d9b0314fbb3e8d59e083dcfea37bdd3aa35a
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-13 20:02:21 -0300

    Update ex070.py

--- Exercicios/ex070.py
+++ Exercicios/ex070.py
@@ -2,7 +2,7 @@ print('\tLOJA SUPER BARATO\n')
 c = 1
 totp = 0
 maiorp = 0
-nomemenor = ''
+nomemenor = ' '
 menorp = 0
 cond = ''
 while True:
@@ -12,9 +12,22 @@ while True:
     precop = int(input('Preço: '))
     if precop > 1000:
         maiorp = maiorp + 1
-    if menorp > precop:
+    if c == 2 :
         nomemenor = nomep
+        menorp = precop
+    else:
+        if menorp > precop:
+            nomemenor = nomep
+            menorp = precop
+
     cond = str(input('Quer continuar? [S/N] ')).strip().lower()
+    totp = totp + precop
     if cond == 'n':
         break
+
+print('-' * 20)
+print(f'Total:[{totp}]')
+print(f'Produto mais barato:[{nomemenor}]')
+print(f'Quantos produtos maiores que R$1000:[{maiorp}]')
+
 
