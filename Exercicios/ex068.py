207a5673a7bfdf8e1daed69d0c3baf4da036e6fa
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-12 21:51:45 -0300

    Update ex068.py

--- Exercicios/ex068.py
+++ Exercicios/ex068.py
@@ -6,4 +6,8 @@ while True:
     computador = randint(0,10)
     total = computador + jogador
     tipo = ''
-    while tipo not in 'PI'
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Impar: [P/I]')).strip().upper()
+    print(f'Você:[{jogador}]')
+    print(f'Computador:[{computador}]')
+    print(f'Total:[{total}]')
