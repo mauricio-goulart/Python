164c659933cda26915677e5dcc456cb3b7273855
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-14 19:36:33 -0300

    Update ex076.py

---
+++ Exercicios/ex076.py
@@ -0,0 +1,7 @@
+listagem = ('RTX 2060',1500,
+            'RTX 3070 TI',2900,
+            'RTX 4080 TI',6000,
+            'RYZEN 7 2700',1000,
+            'INTEL I7 7700',1500,
+            'HOGWARTS LEGACY PC',250,
+            'HOGWARTS LEGACY DELUXE PC,',300)
