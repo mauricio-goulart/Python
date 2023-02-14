42ae5795b18a0013ae11ab78f30ec078116ae042
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-14 19:43:05 -0300

    Update ex076.py

--- Exercicios/ex076.py
+++ Exercicios/ex076.py
@@ -4,4 +4,14 @@ listagem = ('RTX 2060',1500,
             'RYZEN 7 2700',1000,
             'INTEL I7 7700',1500,
             'HOGWARTS LEGACY PC',250,
-            'HOGWARTS LEGACY DELUXE PC,',300)
+            'HOGWARTS LEGACY DELUXE PC',300)
+print('-' * 40)
+print(f'{"LOJA DO MAURICIO":^40}')
+print('-' * 40)
+
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
+print('-' * 40)
