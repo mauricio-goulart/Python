0baf17be15512f7025c9ad620602ec7f6dcd7087
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-14 19:09:05 -0300

    Update ex075.py

--- Exercicios/ex075.py
+++ Exercicios/ex075.py
@@ -1,14 +1,17 @@
 print('-=' * 20)
 print('\tDIGITE 5 NÚMEROS')
 print('-=' * 20)
+par = 0
 
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
-n3 = int(input('Digite outro número: '))
-n4 = int(input('Digite outro número: '))
-n5 = int(input('Digite outro número: '))
+num = (int(input('Digite um número: ')),
+       int(input('Digite outro número: ')),
+       int(input('Digite outro número: ')),
+       int(input('Digite outro número: ')),
+       int(input('Digite outro número: ')))
 print('-=' * 20)
 
-numeros = [n1, n2, n3, n4, n5]
+print(f'O valor 9 apareceu: [{num.count(9)}] Vezes')
 
-print(f'O valor [9] apareceu:[{numeros.count(9)}] Vezes')
+
+
+
