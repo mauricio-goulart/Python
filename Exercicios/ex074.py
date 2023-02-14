d436582bb7992caa8d946f71ce9338a5666abac6
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-14 18:34:10 -0300

    Update ex074.py

--- Exercicios/ex074.py
+++ Exercicios/ex074.py
@@ -2,6 +2,14 @@ from random import randint
 
 n = [randint(0,5), randint(0,5), randint(0,5), randint(0,5), randint(0,5)]
 
+print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+print('Número sorteados: ', end='')
 for numero in n:
-    print(numero, end='')
+    print(f'{numero}', end=' ')
+
+print('\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+print(f'Maior valor:[{max(n)}]')
+print(f'Menor valor:[{min(n)}]')
+print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+
 
