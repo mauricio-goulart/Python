12bd593566edefdec497aeaa16a342e6428d94ef
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-14 18:57:14 -0300

    Update ex075.py

--- Exercicios/ex075.py
+++ Exercicios/ex075.py
@@ -7,7 +7,8 @@ n2 = int(input('Digite outro número: '))
 n3 = int(input('Digite outro número: '))
 n4 = int(input('Digite outro número: '))
 n5 = int(input('Digite outro número: '))
+print('-=' * 20)
 
 numeros = [n1, n2, n3, n4, n5]
 
-print()
+print(f'O valor [9] apareceu:[{numeros.count(9)}] Vezes')
