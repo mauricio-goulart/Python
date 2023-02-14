963c1241ecbf024acf56afb5468acd20745b7a15
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-14 18:26:55 -0300

    Update ex074.py

--- Exercicios/ex074.py
+++ Exercicios/ex074.py
@@ -1,10 +1,7 @@
 from random import randint
-n1 = randint(0,5)
-n2 = randint(0,5)
-n3 = randint(0,5)
-n4 = randint(0,5)
-n5 = randint(0,5)
 
-n = [n1, n2, n3, n4, n5]
+n = [randint(0,5), randint(0,5), randint(0,5), randint(0,5), randint(0,5)]
 
-print(n)
+for numero in n:
+    print(numero, end='')
+
