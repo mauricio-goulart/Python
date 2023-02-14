fd7bfc12185bc740c78d88da0e9945b0d4938d97
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-14 17:00:00 -0300

    Update ex073.py

--- Exercicios/ex073.py
+++ Exercicios/ex073.py
@@ -1,1 +1,3 @@
 brasileirao = ['Palmeiras','Internacional','Fluminense','Corinthians','Flamengo','Athletico-PR','Atlético-MG','Fortleza','São Paulo','América-MG','Botafogo','Santos','Goiás','Bragantino','Coritiba','Cuiabá','Ceará SC','Atlético-GO','Avaí','Juventude']
+print(f'{"Brasileirão-22":=^30}')
+print(f'Primeiros 5 colocados:{brasileirao[0:6]}')
