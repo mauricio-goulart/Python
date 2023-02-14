ffe6f6ad02fc4fadb69fc2684afaf2ebe02eb250
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-14 18:55:05 -0300

    Update ex075.py

--- Exercicios/ex075.py
+++ Exercicios/ex075.py
@@ -1,3 +1,13 @@
-print('-=' * 10)
+print('-=' * 20)
 print('\tDIGITE 5 NÚMEROS')
-print('-=' * 10)
+print('-=' * 20)
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+n3 = int(input('Digite outro número: '))
+n4 = int(input('Digite outro número: '))
+n5 = int(input('Digite outro número: '))
+
+numeros = [n1, n2, n3, n4, n5]
+
+print()
