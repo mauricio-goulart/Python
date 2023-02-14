d341d0b9cf521368907e091ba068c6d1536f596b
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-14 16:26:54 -0300

    Update ex072.py

--- Exercicios/ex072.py
+++ Exercicios/ex072.py
@@ -1,3 +1,8 @@
 numero = ['zero','um','dois','três','quatro','cinco','seis','sete','oito','nove','dez','onze','treze','catorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte']
 n1 = int(input('Digite um número de 0 a 20: '))
+
+while n1 > 20:
+    print('Digite um número valído')
+    n1 = int(input('Digite um número de 0 a 20: '))
+
 print(f'[{n1}] = [{numero[n1]}]')
