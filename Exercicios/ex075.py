216c24ce0a5262d3282280c7fb46e107938d32d5
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-14 19:17:42 -0300

    Update ex075.py

--- Exercicios/ex075.py
+++ Exercicios/ex075.py
@@ -11,6 +11,16 @@ num = (int(input('Digite um número: ')),
 print('-=' * 20)
 
 print(f'O valor 9 apareceu: [{num.count(9)}] Vezes')
+if 3 in num:
+    print(f'O valor 3 apareceu na: [{num.index(3)}]ª Posição')
+else:
+    print(f'O valor 3 apareceu não apareceu')
+print('Valores pares: ', end='')
+for n in num:
+    if n % 2 == 0:
+        print({n}, end='')
+
+
 
 
 
