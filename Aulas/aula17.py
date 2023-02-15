da5dbd61f3b25a9fb64910ea7cad052a2fa4669e
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-15 16:18:26 -0300

    Update aula17.py

--- Aulas/aula17.py
+++ Aulas/aula17.py
@@ -1,6 +1,10 @@
-num = [1 , 2, 3]
-num[2] = 999
-num.append(7)
-num.sort()
-num.insert(2,4)
-print(num)
+#num = [1 , 2, 3]
+#num[2] = 999
+#num.append(7)
+#num.sort()
+#num.insert(2,4)
+#print(num)
+jogos = ['Hogwarts Legacy', 'The Witcher 3', 'Cyberpunk 2077', 'Skyrim']
+
+for jogo in jogos:
+    print(f'{jogo} - ', end='')
