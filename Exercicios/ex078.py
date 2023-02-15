7c93f46ab4bf5c5a9eff384fb35aafa15b2f260e
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-15 16:45:54 -0300

    Update ex078.py

--- Exercicios/ex078.py
+++ Exercicios/ex078.py
@@ -1,4 +1,12 @@
 numeros = list()
 
-for c in range(0,6):
+for c in range(0,5):
     numeros.append(int(input(f'Digite um valor para a posição {c}: ')))
+
+print('-=' * 20)
+print('Você digitou os números: ', end='')
+for n in numeros:
+    print(f' {n} ',end='')
+
+print(f'\nMaior número:[{max(numeros)}] nas posições')
+print(f'\nMenor número:[{min(numeros)}] nas posições')
