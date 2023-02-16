8ce423e5ad05ad63512ba83a2196430c852685e0
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-15 22:49:49 -0300

    Update ex082.py

--- Exercicios/ex082.py
+++ Exercicios/ex082.py
@@ -5,6 +5,16 @@ cond = ''
 
 while True:
     n = int(input('Digite um valor: '))
+    lista.append(n)
+    if n % 2 == 0:
+        listapar.append(n)
+    else:
+        listaimpar.append(n)
     cond = str(input('Quer continuar: [S/N] '))
+    print('-=' * 15)
     if cond in 'Nn':
         break
+print(f'Lista completa:{lista}')
+print(f'Lista pares:{listapar}')
+print(f'Lista impares:{listaimpar}')
+print('-=' * 15)
