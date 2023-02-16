7a1fb07181c4728e259a89712e307b9d91099a01
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-15 22:10:34 -0300

    Update ex079.py

--- Exercicios/ex079.py
+++ Exercicios/ex079.py
@@ -4,6 +4,7 @@ while True:
 
     for n in numeros:
         numeros.append(int(input('Digite um valor: ')))
+        
     cond = str(input('Quer continuar: [S/N] '))
     if cond in 'Nn':
         print('-=' * 15)
