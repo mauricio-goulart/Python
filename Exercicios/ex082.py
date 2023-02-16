9093d6655c1e88323f8178ec7360891642218fde
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-15 22:46:38 -0300

    Update ex082.py

---
+++ Exercicios/ex082.py
@@ -0,0 +1,10 @@
+lista = []
+listapar = []
+listaimpar = []
+cond = ''
+
+while True:
+    n = int(input('Digite um valor: '))
+    cond = str(input('Quer continuar: [S/N] '))
+    if cond in 'Nn':
+        break
