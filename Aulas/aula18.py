94aecca21fc7a454e5c11962142b5ef6655849a2
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-16 22:13:34 -0300

    Update aula18.py

--- Aulas/aula18.py
+++ Aulas/aula18.py
@@ -3,3 +3,13 @@ print(pessoas[1])
 
 for p in pessoas:
     print(f'{p[0]} tem {p[1]} anos de idade')
+
+
+
+galera = list()
+dado = list()
+
+for c in range(0,3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])
