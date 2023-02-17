d28530a8c19d8cde4c353b5d9fa81a8f4cdbc9d7
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-16 22:18:53 -0300

    Update ex084.py

---
+++ Exercicios/ex084.py
@@ -0,0 +1,12 @@
+cond = ''
+pessoas = list()
+gordos = list()
+magros = list()
+c = 0
+
+while True:
+    pessoas.append(str(input('Nome: ')))
+    pessoas.append(int(input('Idade: ')))
+    c = c + 1
+    
+
