74d55d1cf38f0f02a675e12a99e8c7963c6de4d1
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-16 22:28:42 -0300

    Update ex084.py

--- Exercicios/ex084.py
+++ Exercicios/ex084.py
@@ -1,12 +1,25 @@
 cond = ''
 pessoas = list()
-gordos = list()
-magros = list()
+t = list()
+mai = men = 0
+
 c = 0
 
 while True:
     pessoas.append(str(input('Nome: ')))
-    pessoas.append(int(input('Idade: ')))
+    pessoas.append(int(input('Peso: ')))
     c = c + 1
-    
+    if len(t) == 0:
+        mai = men = pessoas[1]
+    else:
+        if pessoas[1] > mai:
+            mai = pessoas[1]
+        if pessoas[1] < men:
+            men = pessoas[1]
+    t.append(pessoas[:])
+    pessoas.clear()
+    cond = input('Quer continuar: [S/N] ')
+    if cond in 'Nn':
+        break
+
 
