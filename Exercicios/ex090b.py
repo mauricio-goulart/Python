cbc520d9e25a4f8b0823e2f0db8ae9d823a2c215
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-01 10:21:55 -0300

    Update ex090b.py

---
+++ Exercicios/ex090b.py
@@ -0,0 +1,24 @@
+cidade = []
+lista = []
+cont = 1
+while True:
+    print('-=' * 3, f'{cont}ºLOJA', '-=' * 3)
+    cid = str(input('Cidade: ')).capitalize().strip()
+    lista.append(cid)
+    fatu = float(input('Faturamento: R$'))
+    lista.append(fatu)
+    cidade.append(lista[:])
+    lista.clear()
+    cont = cont + 1
+    cond = str(input('--> Quer continuar? [S/n]')).strip()
+    if cond in 'Nn':
+        break
+
+print('-=' * 4, 'ANALISADO', '-=' * 4)
+for c in cidade:
+    for x in c:
+        print(f'{x}')
+
+
+
+
