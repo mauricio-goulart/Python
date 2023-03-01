12770e7719e8ab31360eee3fe2bb214d5cdebda8
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-28 21:11:31 -0300

    Update ex085.py

--- Exercicios/ex085.py
+++ Exercicios/ex085.py
@@ -3,15 +3,17 @@ totp = 0
 toti = 0
 valor = 0
 
-print(f'{"IMPAR OU PAR":=^20}')
+print(f'\n{"IMPAR OU PAR":=^20}')
 
 for c in range (1,8):
     valor = int(input(f'Digite o {c}º valor: '))
     if valor % 2 == 0:
         num[0].append(valor)
+        totp = totp + 1
     else:
         num[1].append(valor)
-
+        toti = toti + 1
+print(f'{"Analisado":=^20}')
 
 print(f'Valores pares: {num[0]}')
 print(f'Valores Impares: {num[1]}')
