8bf59ff565f4965213f2f0073269d9f80194c5d9
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-01 08:54:49 -0300

    Update ex090.py

--- Exercicios/ex090.py
+++ Exercicios/ex090.py
@@ -1,3 +1,16 @@
-print(f'{"FATURAMENTO LOJAS":-^40}')
 
-while
+lista = [[], []]
+cont = 1
+while True:
+    print('-=' * 3, f'{cont}ºLOJA', '-=' * 3)
+    cid = str(input('Cidade: ')).capitalize().strip()
+    lista[0].append(cid)
+    fatu = float(input('Faturamento: R$'))
+    lista[1].append(fatu)
+    cont = cont + 1
+    cond = str(input('--> Quer continuar? [S/n]')).strip()
+    if cond in 'Nn':
+        break
+
+print(lista)
+
