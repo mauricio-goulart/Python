10c8009221d85cb5cef947c94dc7e4fb2461e75c
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-28 21:09:28 -0300

    Update ex085.py

--- Exercicios/ex085.py
+++ Exercicios/ex085.py
@@ -1,14 +1,17 @@
 num = [[], []]
-
+totp = 0
+toti = 0
 valor = 0
 
+print(f'{"IMPAR OU PAR":=^20}')
+
 for c in range (1,8):
-    valor = int(input('Digite um valor: '))
+    valor = int(input(f'Digite o {c}º valor: '))
     if valor % 2 == 0:
         num[0].append(valor)
     else:
         num[1].append(valor)
 
 
-print(f'Valores pares:{num[0]}')
-print(f'Valores Impares:{num[1]}')
+print(f'Valores pares: {num[0]}')
+print(f'Valores Impares: {num[1]}')
