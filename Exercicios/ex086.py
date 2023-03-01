53ee84f0fcd233247606b1458e3713a31d002894
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-28 21:27:05 -0300

    Update ex086.py

--- Exercicios/ex086.py
+++ Exercicios/ex086.py
@@ -2,6 +2,6 @@ matriz = [[0,0,0], [0,0,0], [0,0,0]]
 
 for l in range (0,3):
     for c in range (0,3):
-        matriz[l][c] = int(input(f'Digite um valor para [{l},{c}]: '))
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
 
 print(matriz)
