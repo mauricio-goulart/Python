dab1f60c0f5920d99aa5957311fe2beba9fa8f74
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-28 21:39:55 -0300

    Update ex087.py

--- Exercicios/ex087.py
+++ Exercicios/ex087.py
@@ -16,4 +16,9 @@ print(f'A soma dos valores pares é {spar}')
 for l in range(0, 3):
     scol += matriz[l][2]
 print(f'A soma dos valores da terceira coluna:[{scol}]')
-
+for c in range(0, 3):
+    if c == 0:
+        mai = matriz[1][c]
+    elif matriz[1][c] > mai:
+        mai = matriz[1][c]
+print(f'O maior valor da segunda linha: [{mai}]')
