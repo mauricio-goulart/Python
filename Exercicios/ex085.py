1476463f83cf58b6c4e9e4e6364fba23f133e428
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-28 21:04:22 -0300

    Update ex085.py

--- Exercicios/ex085.py
+++ Exercicios/ex085.py
@@ -1,4 +1,14 @@
-cont = 0
+num = [[], []]
 
-for cont range in (0,7):
-    
+valor = 0
+
+for c in range (1,8):
+    valor = int(input('Digite um valor: '))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+
+
+print(f'Valores pares:{num[0]}')
+print(f'Valores Impares:{num[1]}')
