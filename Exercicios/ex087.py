13aa3f2183e5723350a2314e00acdd317234a4bf
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-28 21:35:17 -0300

    Update ex087.py

--- Exercicios/ex087.py
+++ Exercicios/ex087.py
@@ -1,4 +1,5 @@
 matriz = [[0,0,0], [0,0,0], [0,0,0]]
+spar = mai = scol = 0
 
 for l in range (0,3):
     for c in range (0,3):
@@ -7,4 +8,8 @@ print('-=' * 20)
 for l in range(0, 3):
     for c in range(0, 3):
         print(f'[{matriz[l][c]:^5}]', end='')
+        if matriz[l][c] % 2 == 0:
+            spar = spar + matriz[l][c]
     print()
+print('-=' * 30)
+
