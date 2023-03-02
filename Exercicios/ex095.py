9b0dc7731457dc0b728043de4087e9e48b501ec7
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-02 19:19:20 -0300

    Update ex095.py

--- Exercicios/ex095.py
+++ Exercicios/ex095.py
@@ -1,23 +1,43 @@
+time = list()
 jogador = dict()
 gols = list()
-tot = 0
-totg = 0
-print('-=' * 14)
-jogador['Nome'] = str(input('Nome do Jogador: '))
-jogos = int(input(f'Quantos partidas {jogador["Nome"]} jogou: '))
-for c in range(1, jogos + 1):
-    tot = int(input(f'Gols na partida [{c}]: '))
-    gols.append(tot)
-    totg = tot + totg
 
-jogador['Total'] = totg
-jogador['Gols'] = gols
-print('-=' * 15)
+while True:
+    jogador["nome"] = str(input("Informe o nome do Jogador! "))
+    jogador["partidas"] = int(input("Informe o numero de partidas! "))
 
-for k,v in jogador.items():
-    print(f'{k} = {v}')
-print('-=' * 15)
-print(f'O jogador {jogador["Nome"]} jogous {jogos} partidas.')
-for i,g in enumerate(gols):
-    print(f'--> Partida {i} fez {g} gols')
-print('-=' * 15)
+    for c in range(0, jogador["partidas"]):
+        gols.append(int(input("Quantos gols {} fez na partida {}? ".format(jogador["nome"], c))))
+
+    jogador["gols"] = gols[:]
+    jogador['totgols'] = sum(gols)
+    gols.clear()
+
+    time.append(jogador.copy())
+
+    flag = str(input("Deseja continuar? S/N ")).strip().upper()[0]
+    while flag not in "SN":
+        flag = str(input("INVALIDO! Deseja continuar? S/N ")).strip().upper()[0]
+
+    if flag == 'N':
+        break
+
+print("=" * 50)
+print("COD Nome            Gols               Total")
+for i, v in enumerate(time):
+    print("{: >3} {: <16}{: <18} {: >5}".format(i, v["nome"], str(v["gols"]), v["totgols"]))
+print("=" * 50)
+
+while True:
+    flag = int(input("Mostrar dados de quel Jogador? (999 Para!) "))
+    while flag >= len(time):
+        if flag == 999:
+            break
+        flag = int(input("Jogador INVALIDO! (999 Para!) "))
+
+    if flag == 999:
+        break
+    else:
+        print("{:-^50}".format("Levantamento do Jogador {}".format(time[flag]["nome"])))
+        for c, v in enumerate(time[flag]["gols"]):
+            print("No jogo {} {} fez {} gols!".format(c, time[flag]["nome"], v))
