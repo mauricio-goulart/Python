7a8c209180e75effd02b0d7b3b358a0dbd851713
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-03 09:14:43 -0300

    Update ex096.py

---
+++ Exercicios/ex096.py
@@ -0,0 +1,16 @@
+def area(n1,n2):
+    n1 * n2
+    print(f'{n1} x {n2}')
+    print(f'Área = {n1 * n2}')
+
+
+print('-=' * 15)
+print('\tControle de Terrenos')
+print('-=' * 15)
+
+l = float(input('LARGURA (m): '))
+a = float(input('ALTURA (m): '))
+print('-=' * 15)
+
+area(l,a)
+
