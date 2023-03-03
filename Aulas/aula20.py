df07975c3a74661d5ccb5bb132d672f89194ea1d
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-02 23:12:39 -0300

    Update aula20.py

--- Aulas/aula20.py
+++ Aulas/aula20.py
@@ -4,5 +4,21 @@ def titulo(txt):
     print('-=' * 30)
 
 
-titulo('HARRY POTTER')
+
 titulo('PREENDA-ME SE FOR CAPAZ')
+
+def soma(a,b):
+    s = a + b
+    print(s)
+
+
+soma(4,5)
+soma(b = 5, a = 4)
+
+def contador(*num):
+
+    print(f'Recebi os valores {num} e tem {len(num)} numeros')
+
+
+contador(5,3,1,4)
+contador(4,2,1)
