715513a67eee2b4a018e34180a82475b1be1a9b3
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-03 10:08:34 -0300

    Update ex098.py

--- Exercicios/ex098.py
+++ Exercicios/ex098.py
@@ -1,6 +1,9 @@
+from time import sleep
+
 def cont(c1,c2,c3):
     for con in range(c1,c2 + 1,c3):
         print(f'{con}', end=' ')
+        sleep(0.5)
     print('FIM!')
 
 
@@ -8,11 +11,13 @@ print('-=' * 20)
 print('Contagem de 1 até 10 de 1 em 1')
 for c in range(1,11):
     print(f'{c}', end=' ')
+    sleep(0.5)
 print('FIM!')
 print('-=' * 20)
 print('Contagem de 10 até 0 de 2 em 2')
 for c in range(10,-1,-2):
     print(f'{c}', end=' ')
+    sleep(0.5)
 print('FIM!')
 print('-=' * 20)
 print('Agora é a sua vez de personalizar a contagem!')
