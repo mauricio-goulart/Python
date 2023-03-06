330105f625eb3f1942f82f43942ec1d97838c3e3
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-06 10:21:08 -0300

    Update ex105.py

--- Exercicios/ex105.py
+++ Exercicios/ex105.py
@@ -1,10 +1,22 @@
-def notas(*num):
+def notas(*num, sit = False):
     alunos = dict()
-    
+    alunos['total'] = len(num)
+    alunos['maior'] = max(num)
+    alunos['menor'] = min(num)
+    alunos['media'] = sum(num) / len(num)
+    if sit:
+        if alunos['media'] >= 6.5:
+            alunos['situacao'] = 'Aprovado'
+        else:
+            alunos['situacao'] = 'Reprovado'
+
+
+
+    return alunos
 
 
 
 
-
-n = notas(8,9,10)
+print('-' * 30)
+n = notas(8, 9, sit= True)
 print(n)
