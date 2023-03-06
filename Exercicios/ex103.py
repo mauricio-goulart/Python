18a87a97a4fae71fca2572f63ef36a1c18cb6206
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-05 22:09:29 -0300

    Update ex103.py

---
+++ Exercicios/ex103.py
@@ -0,0 +1,21 @@
+def ficha(jogador = '', gols = 0):
+
+    if jogador == '':
+        print('Jogador: <desconhecido>')
+        print(f'Gols: {gols}')
+    elif gols == 0:
+        print(f'Jogador: {jogador}')
+        print(f'Gols: 0')
+    elif jogador == '' and gols == 0:
+        print(f'Jogador: <desconhecido>')
+        print(f'Gols: 0')
+    else:
+        print(f'Jogador: {jogador}')
+        print(f'Gols: {gols}')
+
+
+print('-=' * 15)
+nome = str(input('Nome do Jogador: '))
+gols = int(input('Gols: '))
+print('-=' * 15)
+ficha(nome,gols)
