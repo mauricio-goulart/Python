6587ea85d88fe6b01514b0aee6dda2c56615eee6
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-08 20:30:07 -0300

    Update teste.py

--- Exercicios/ex107/teste.py
+++ Exercicios/ex107/teste.py
@@ -1,2 +1,11 @@
+import moeda
 
+print('-=' * 15)
 m = float(input('Digite o preço: '))
+print('-=' * 15)
+print(f'A metade de [{m}]: {moeda.metade(m):.2f}')
+print(f'O dobro de [{m}]: {moeda.dobro(m)}')
+print(f'O triplo de [{m}]: {moeda.triplo(m)}')
+print(f'Aumentando 10% de [{m}]: {moeda.aumenta(m, 10)}')
+print(f'Retirando 13% de [{m}]: {moeda.retira(m,13)}')
+print('-=' * 15)
