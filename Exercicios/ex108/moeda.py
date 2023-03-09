291a5d2c4669000b7d9007275fca47d8255f7cad
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-09 07:18:13 -0300

    Create moeda.py

---
+++ Exercicios/ex108/moeda.py
@@ -0,0 +1,22 @@
+def metade(n):
+    m = n /2
+    return m
+
+
+def dobro(n):
+    m = n * 2
+    return m
+
+
+def triplo(n):
+    m = n * 3
+    return m
+
+
+def aumenta(n, p):
+    m = (n * p) / 100 + n
+    return m
+
+def retira(n , p):
+    m = n - (n * p) / 100
+    return m
