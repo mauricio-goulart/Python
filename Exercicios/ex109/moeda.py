e5bfd66dac4682164cc164ace0fcca52dfc6ba67
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-09 08:00:47 -0300

    Update moeda.py

--- Exercicios/ex109/moeda.py
+++ Exercicios/ex109/moeda.py
@@ -1,25 +1,35 @@
-def metade(n):
+def metade(n, f = False):
     m = n /2
+    if f:
+        return f'R${m}'.replace('.',',')
     return m
 
 
-def dobro(n):
+def dobro(n, f = False):
     m = n * 2
+    if f:
+        return f'R${m}'.replace('.',',')
     return m
 
 
-def triplo(n):
+def triplo(n, f = False):
     m = n * 3
+    if f:
+        return f'R${m}'.replace('.',',')
     return m
 
 
-def aumenta(n=0, p=0):
+def aumenta(n=0, p=0, f = False):
     m = (n * p) / 100 + n
+    if f:
+        return f'R${m}'.replace('.',',')
     return m
 
 
-def retira(n=0 , p=0):
+def retira(n=0 , p=0, f = False):
     m = n - (n * p) / 100
+    if f:
+        return f'R${m}'.replace('.',',')
     return m
 
 
