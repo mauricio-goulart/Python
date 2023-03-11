557d6dd0f03d44bd0564a4b967a00fc97539f01a
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-11 12:40:12 -0300

    Update aula22.py

--- Aulas/aula22.py
+++ Aulas/aula22.py
@@ -2,7 +2,9 @@ try:
     a = int(input('Digite o numerador: '))
     b = int(input('Digite o denominador: '))
     c = a/b
+    print('-=' * 15)
 except:
+    print('-=' * 15)
     print('Tivemos um problema!')
 
 else:
