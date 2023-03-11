e57d0f3e2b81833a651567e0918f66ff6cb9684b
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-11 12:39:01 -0300

    Update aula22.py

--- Aulas/aula22.py
+++ Aulas/aula22.py
@@ -1,6 +1,9 @@
+try:
+    a = int(input('Digite o numerador: '))
+    b = int(input('Digite o denominador: '))
+    c = a/b
+except:
+    print('Tivemos um problema!')
 
-a = int(input('Digite o numerador: '))
-b = int(input('Digite o denominador: '))
-c = a/b
-
-print(f'{c}')
+else:
+    print(f'{c}')
