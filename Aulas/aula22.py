7ab9a3630deb4e6368c876894f100ef9ab51c727
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-11 12:49:10 -0300

    Update aula22.py

--- Aulas/aula22.py
+++ Aulas/aula22.py
@@ -3,9 +3,17 @@ try:
     b = int(input('Digite o denominador: '))
     c = a/b
     print('-=' * 15)
-except:
+except ZeroDivisionError:
     print('-=' * 15)
-    print('Tivemos um problema!')
-
+    print('Tivemos um problema! Não se divide algo por 0 !')
+except (ValueError, TypeError):
+    print('-=' * 15)
+    print('Tivemos um problema! Digite um valor valido!')
+except KeyboardInterrupt:
+    print('-=' * 15)
+    print('Tivemos um problema! Vc abandonou o programa')
 else:
     print(f'{c}')
+
+finally:
+    print('Volte sempre')
