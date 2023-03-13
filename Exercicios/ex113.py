3ebe9befbef12901e9e1bcec07d8f73db679edc2
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-12 21:59:59 -0300

    Update ex113.py

--- Exercicios/ex113.py
+++ Exercicios/ex113.py
@@ -11,13 +11,26 @@ def LeiaInt(msg):
 
 
 
+def LeiaFloat(msg):
+    while True:
+        try:
+            f = float(input(msg))
+        except:
+            print('-=' * 15)
+            print('\033[31mERRO: NUMERO INVALIDO\033[m')
+            continue
+        else:
+            return f
+
+
 
 
 
 print('-=' * 15)
-num = LeiaInt('Digite um valor: ')
+num = LeiaInt('Digite um valor inteiro: ')
+float = LeiaFloat('Digite um valor real: ')
 print('-=' * 15)
-print(f'\033[32mO valor digitado foi {num}\033[m')
+print(f'\033[32mO valor digitado foi {num} e {float}\033[m')
 
 
 
